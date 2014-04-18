29711414fd6fed4866220e70d7b0a7838c4290e4
wccah <william.c.cahill@gmail.com>
2014-04-17 23:42:52 -0400

    .

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl1', [
+  	"$scope", 
+  	function($scope) {
+		$scope.cards = [{name:"USAir"}];
   }]);
---
+++ server.js
@@ -0,0 +1,58 @@
+var restify = require('restify');  
+var restMongoose = require('restify-mongoose');
+var mongoose = require('mongoose/');
+var config = require('./config');
+db = mongoose.connect(config.creds.mongoose_auth),
+Schema = mongoose.Schema;
+
+/*
+var mongodbServer = restify.createServer({
+    formatters: {
+        'application/json': function(req, res, body){
+            if(req.params.callback){
+                var callbackFunctionName = req.params.callback.replace(/[^A-Za-z0-9_\.]/g, '');
+                return callbackFunctionName + "(" + JSON.stringify(body) + ");";
+            } else {
+                return JSON.stringify(body);
+            }
+        },
+        'text/html': function(req, res, body){
+            return body;
+        }
+    }
+});
+*/
+
+var server = restify.createServer({
+    name: 'restify.mongoose.examples.notes',
+    version: '1.0.0'
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+//mongodbServer.use(restify.bodyParser());
+
+var CardSchema = new Schema({
+	name: String,
+	deadline: Date,
+	reward: Number,
+	rewardUnit: String,
+	minSpend: Number
+});
+
+var Card = mongoose.model('Card', CardSchema);
+
+var cardRM = restMongoose(Card);
+
+server.get('/api/cards', cardRM.query());
+server.get('/api/card/:id', cardRM.detail());
+server.put('/api/card', cardRM.insert());
+server.patch('/ap/card/:id', cardRM.update());
+server.del('/api/card/:id', cardRM.remove());
+
+server.listen(80, function() {
+	console.log('listening');
+});
+
